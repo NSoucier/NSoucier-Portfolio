abe7ce072d45ae7c10ce9cbd0139c34f3b7432bd
Nathan Soucier <soucienp@gmail.com>
2024-08-23 14:39:06 -0400

    made basic template of portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggleModule, MatToolbarModule, MatGridListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'nsoucier!!!';
+  title = 'nsoucier?!?';
 }
